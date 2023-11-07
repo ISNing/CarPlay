1f0a19959127744777d561e07ffeb0c43dd01c0a
ISNing <1208733578@qq.com>
2023-11-07 20:27:04 +0800

    timer: Higher freq

--- timer.c
+++ timer.c
@@ -1,23 +1,28 @@
 #include "timer.h"
 
 unsigned char time_lock = 0;
-unsigned long time_us = 0;
+unsigned long time_hns = 0;
 
 void check_reset() {
     if (time_lock == 0) {
-        time_us = 0; // reset to 0
+        time_hns = 0; // reset to 0
     }
 }
 
-void delay_us(unsigned short us) {
-    long init = time_us;
+void delay_hns(unsigned short hns) {
+    long init = time_hns;
     check_reset();
     time_lock++;
-    while ((time_us - init) < us)
+    while ((time_hns - init) < hns)
         ;
     time_lock--;
     check_reset();
 }
+
+void delay_us(unsigned short us) {
+    delay_hns(10 * us);
+}
+
 void delay_ms(unsigned long ms) {
     unsigned long x;
     for (x = 0; x < ms; x++)
@@ -32,6 +37,6 @@ void timer_reset() {
 void timer0() interrupt 1 using 2 {
     timer_reset();
     if (time_lock) {
-        time_us++;
+        time_hns++;
     }
 }
