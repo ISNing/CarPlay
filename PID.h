cdc49a12d6e7d10470aa383c9d71d1f9a2da40c4
ISNing <1208733578@qq.com>
2023-11-09 21:59:24 +0800

    pid: fixed

--- PID.h
+++ PID.h
@@ -2,16 +2,29 @@
 #define _PID_H_
 
 struct PID {
-    float kp, ki, kd;
+    int kp, ki, kd;
 };
 typedef struct PID _PID;
 
 struct HistoryError {
-    float integral_bias, last_bias;
+    int integral_bias, last_bias;
 };
 typedef struct HistoryError _HistoryError;
 
-signed char PWMMotorL(signed char TraceData/*, float TarSpeed*/);
-signed char PWMMotorR(signed char TraceData/*, float TarSpeed*/);
+signed char PWMMotorL();
+signed char PWMMotorR();
+void CalcTurnPwm(signed char TraceData);
+
+#define CalcWithPID(res, bias, pid, his)                                       \
+    his.integral_bias += bias;                                                 \
+    res = pid.kp * bias + pid.ki * his.integral_bias +                         \
+          pid.kd * (bias - his.last_bias);                                     \
+    his.last_bias = bias;
+
+#define DEF_PWM_L 10
+#define DEF_PWM_R 10
+#define TURN_KP 3
+#define TURN_KI 0
+#define TURN_KD 0
 
 #endif
--- pid.c
+++ pid.c
@@ -1,24 +1,15 @@
 #include "pid.h"
-#include <utility>
 
 // 定义转向pid参数结构体
-_PID TurnPdate = {100, 0, -34};
+_PID TurnPID = {TURN_KP, TURN_KI, TURN_KD};
 
 // 定义速度pid参数结构体
-_PID SpdPdate = {-50, -6, 0};
+// xdata _PID SpdPID = {15, -3, 0};
 
 _HistoryError TurnHis = {0, 0};
 
-_HistoryError SpdHis = {0, 0};
-
-int CalcWithPID(float bias, _PID pid, _HistoryError *his) {
-    static float xdata res;
-    his->integral_bias += bias;
-    res = pid.kp * bias + pid.ki * his->integral_bias +
-          pid.kd * (bias - his->last_bias);
-    his->last_bias = bias;
-    return res;
-}
+// xdata _HistoryError SpdHis = {0, 0};
+static signed char turn_pwm = 0;
 
 // should ki be restricted?
 // int PositionPIDToSpd(float deviation, _PID pid) {
@@ -27,7 +18,7 @@ int CalcWithPID(float bias, _PID pid, _HistoryError *his) {
 //     Bias = deviation;      // 计算偏差
 //     Integral_bias += Bias; // 求出偏差的积分
 //     pwmKI = Position_KI * Integral_bias;
-//     // if (pwmKI > MAX_MOTOR_PWM) 
+//     // if (pwmKI > MAX_MOTOR_PWM)
 //     //     Integral_bias = MAX_MOTOR_PWM / Position_KI;
 //     Pwm = Position_KP * Bias + pwmKI +
 //           Position_KD * (Bias - Last_Bias); // 位置式PID控制器
@@ -35,54 +26,34 @@ int CalcWithPID(float bias, _PID pid, _HistoryError *his) {
 //     return Pwm;
 // }
 
-int ChangeTraceTurn(int TraceData) {
-    int pwm = 0;
-    int bias;
-    bias = TraceData;
-    // pwm = CalcWithPID(bias, TurnPdate, &TurnHis);
-    return pwm;
+void CalcTurnPwm(signed char TraceData) {
+    CalcWithPID(turn_pwm, TraceData, TurnPID, TurnHis);
 }
 
-int ChangeSpeedMotorL(int NowEncodeSpdL, float TarSpdL) {
-    int pwm = 0;
-    int bias;
-    int TarEncodeSpdL;
-    // TarEncodeSpdL =
-    //     (int)((TarSpdL * ACircleEncoder) / (WheelOneCircleDis * 100) +
-    //           0.5f); // 根据目标速度求出目标编码器速度
-    bias = NowEncodeSpdL - TarEncodeSpdL;
-    pwm = CalcWithPID(bias, SpdPdate, &SpdHis);
-    return pwm;
-}
+// int ChangeSpeedMotorL(int NowEncodeSpdL, float TarSpdL) {
+//     idata signed char pwm = 0;
+//     idata signed char bias;
+//     idata signed char TarEncodeSpdL;
+//     // TarEncodeSpdL =
+//     //     (int)((TarSpdL * ACircleEncoder) / (WheelOneCircleDis * 100) +
+//     //           0.5f); // 根据目标速度求出目标编码器速度
+//     bias = NowEncodeSpdL - TarEncodeSpdL;
+//     pwm = CalcWithPID(bias, &SpdPID, &SpdHis);
+//     return pwm;
+// }
 
-int ChangeSpeedMotorR(int NowEncodeSpdR, float TarSpdR) {
-    int pwm = 0;
-    int bias;
-    int TarEncodeSpdR;
-    // TarEncodeSpdR =
-    //     (int)((TarSpdR * ACircleEncoder) / (WheelOneCircleDis * 100) +
-    //           0.5f); // 根据目标速度求出目标编码器速度
-    bias = NowEncodeSpdR - TarEncodeSpdR;
-    pwm = CalcWithPID(bias, SpdPdate, &SpdHis);
-    return pwm;
-}
+// int ChangeSpeedMotorR(int NowEncodeSpdR, float TarSpdR) {
+//     idata signed char pwm = 0;
+//     idata signed char bias;
+//     idata signed char TarEncodeSpdR;
+//     // TarEncodeSpdR =
+//     //     (int)((TarSpdR * ACircleEncoder) / (WheelOneCircleDis * 100) +
+//     //           0.5f); // 根据目标速度求出目标编码器速度
+//     bias = NowEncodeSpdR - TarEncodeSpdR;
+//     pwm = CalcWithPID(bias, &SpdPID, &SpdHis);
+//     return pwm;
+// }
 
-signed char PWMMotorL(signed char TraceData/*, float TarSpeed*/) {
-    signed char turnpwm=0, spdpwml = 0;
-    // turnpwm = ChangeTraceTurn(TraceData);
+signed char PWMMotorL() { return DEF_PWM_L + turn_pwm; }
 
-    spdpwml = 20;
-    // spdpwml = ChangeSpeedMotorL(curspd, TarSpeed);
-    
-    return turnpwm + spdpwml;
-}
-
-signed char PWMMotorR(signed char TraceData/*, float TarSpeed*/) {
-    signed char turnpwm, spdpwmr = 0;
-    // turnpwm = ChangeTraceTurn(TraceData);
-
-    spdpwmr = 20;
-    // spdpwmr = ChangeSpeedMotorR(curspd, TarSpeed);
-    
-    return spdpwmr;
-}
+signed char PWMMotorR() { return DEF_PWM_R - turn_pwm; }
